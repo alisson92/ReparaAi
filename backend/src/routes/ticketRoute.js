d9b65e6360b354878ebf23b43510942bf142dd59
Alisson Lima <alisson.lima@newm.com.br>
2025-09-14 21:48:29 -0300

    corrigindo

--- backend/src/routes/ticketRoute.js
+++ backend/src/routes/ticketRoute.js
@@ -1,12 +1,16 @@
-const express = require ('express');
-const router = express.Router();
-const TicketController = require ('../controllers/ticketController')
-const TicketControll = new TicketController();
+const { Router } = require('express');
+const ticketController = require('../controllers/ticketController');
+// 1. Importe o nosso novo middleware de autenticação
+const authMiddleware = require('../middlewares/authMiddleware');
 
-router.get("/tickets/" , TicketControll.findAllTickets);
-router.post("/tickets/" , TicketControll.createTicket);
-router.get("/tickets/:idTicket" , TicketControll.findTicketByPk);
-router.put("/tickets/:idTicket" , TicketControll.updateTicket);
-router.delete("/tickets/:idTicket" , TicketControll.deleteTicket);
+const router = Router();
+
+// 2. Adicione o 'authMiddleware' como um passo intermediário em todas as rotas de ticket.
+// A requisição primeiro passará pelo 'authMiddleware' e, se for aprovada, seguirá para o controller.
+router.get('/tickets', authMiddleware, ticketController.findAllTickets);
+router.post('/tickets', authMiddleware, ticketController.createTicket);
+router.get('/tickets/:idTicket', authMiddleware, ticketController.findTicketByPk);
+router.put('/tickets/:idTicket', authMiddleware, ticketController.updateTicket);
+router.delete('/tickets/:idTicket', authMiddleware, ticketController.deleteTicket);
 
 module.exports = router;
